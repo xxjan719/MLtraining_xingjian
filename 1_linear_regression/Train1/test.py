95c201553a4af9c57c8f7e20e14f7856e5f48e08
xxjan719 <xxjan6291@126.com>
2023-08-20 19:29:46 -0400

    Update test.py

--- 1_linear_regression/Train1/test.py
+++ 1_linear_regression/Train1/test.py
@@ -13,9 +13,9 @@ from sklearn.preprocessing import StandardScaler
 House_price_X, House_price_y = datasets.load_boston(return_X_y=True)
 House_price_yy = House_price_y.reshape(np.shape(House_price_y)[0],1)
 data = np.concatenate([House_price_X,House_price_yy],axis=1)
-mm = StandardScaler()# 创建MinMaxScaler 对象
-mm_data = mm.fit_transform(data) # 归一化数据
-origin_data = mm.inverse_transform(mm_data) # 转换成归一化之前的数据
+mm = StandardScaler()# create Object for StandScaler 
+mm_data = mm.fit_transform(data) # normalization for this Boston data
+origin_data = mm.inverse_transform(mm_data) # return back to this Boston data
 
 
 
